9eac6cd2cce51bbfceefef795eb9f9581e6107de
Akinpelumi Akintunde <akintundeakinpelumi@gmail.com>
2020-12-07 13:55:27 +0100

    firebase into practice

---
+++ firebase-testing/functions/index.js
@@ -0,0 +1,18 @@
+const functions = require('firebase-functions');
+const express = require('express');
+
+const app = express();
+
+app.get('/timestamp', (request, response) => {
+    response.send(`${Date.now()} Hello from Firebase!`)
+})
+
+app.get('/timestamp-cached', (request, response) => {
+    response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+    response.send(`${Date.now()} Hello from Firestore!`);
+})
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.app = functions.https.onRequest(app);
